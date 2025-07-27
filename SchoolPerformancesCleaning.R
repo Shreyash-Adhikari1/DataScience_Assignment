b485aaa9acbc0fd082a01cf10a4f4513e1fd6ed3
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2025-07-27 19:58:51 +0545

    School performances grouping logic fix || postcode code fixed

--- SchoolPerformancesCleaning.R
+++ SchoolPerformancesCleaning.R
@@ -1,47 +1,66 @@
 library(tidyverse)
 
-# Base path for where school performances of each year are saved
-base_path = "C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Obtained Data\\SchoolPerformances"
 
-# List performance folders
+base_path = "C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Obtained Data\\SchoolPerformances"
 year_folders = c("2021-2022", "2022-2023", "2023-2024")
 
 
-# Function to read and clean one england_ks4final CSV from a given year folder
+yorkshire_prefixes = c("BD", "DN", "HD", "HG", "HU", "HX", "LS", "S", "WF", "YO")
+
+
+towns = read_csv("C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Cleaned Data\\Cleaned_Town_Population.csv") %>%
+  mutate(
+    shortPostcode = str_to_upper(str_trim(shortPostcode)),
+    County = str_to_upper(str_trim(County))
+  )
+
+towns_yorkshire = towns %>%
+  filter(County %in% c("SOUTH YORKSHIRE", "WEST YORKSHIRE"))
+
 
 read_clean_ks4final = function(year_folder) {
-  folder_path =file.path(base_path, year_folder)
+  folder_path = file.path(base_path, year_folder)
   
-  # Find the CSV file name containing 'england_ks4final'
   csv_file = list.files(folder_path, pattern = "(?i)england_ks4final.*\\.csv$", full.names = TRUE)
-  
-  if (length(csv_file) == 0) {
-    message("No england_ks4final file found in ", year_folder)
-    return(tibble())
-  }
+  if (length(csv_file) == 0) return(tibble())
   
   df = read_csv(csv_file, show_col_types = FALSE)
   
-  # Columns to keep; add more if needed
-  cols_to_keep = c("LEA", "URN", "SCHNAME", "TOWN", "PCODE", "ATT8SCR", "TOTATT8")
+  cols_to_keep = c("LEA", "URN", "SCHNAME", "TOWN", "PCODE", "ATT8SCR")
   cols_to_keep = intersect(cols_to_keep, colnames(df))
   
   df_clean = df %>%
     select(all_of(cols_to_keep)) %>%
     na.omit() %>%
     mutate(
-      shortPostcode = str_trim(substr(PCODE, 1, 4)),
+      
+      shortPostcode = str_to_upper(str_trim(str_extract(PCODE, "^[A-Z0-9]+"))),
       Year = year_folder
-    )
+    ) %>%
+    
+    filter(str_sub(shortPostcode, 1, 2) %in% yorkshire_prefixes)
   
   return(df_clean)
 }
 
-# Read and clean all performances and combined
-combined_ks4final = map_dfr(year_folders, read_clean_ks4final)
 
-# View the combined cleaned data-set 
-View(combined_ks4final)
+ks4_filtered = map_dfr(year_folders, read_clean_ks4final)
 
-# Save the combined cleaned data
-write_csv(combined_ks4final, "C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Cleaned Data\\Cleaned_School_Performances_2021-2024.csv")
+
+combined_ks4final = ks4_filtered %>%
+  left_join(towns_yorkshire, by = "shortPostcode") %>%
+  filter(County %in% c("SOUTH YORKSHIRE", "WEST YORKSHIRE")) %>% 
+  select(
+    LEA, URN, SCHNAME,PCODE, ATT8SCR, shortPostcode, Year,
+    Town, District, County
+  )
+
+if (nrow(combined_ks4final) > 0) {
+  View(combined_ks4final)
+} else {
+  message("No data found for South or West Yorkshire after filtering and join.")
+}
+
+
+write_csv(combined_ks4final,"C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Cleaned Data\\Cleaned_School_Performance.csv"
+)
--- postcodeToLSOAcleaning.R
+++ postcodeToLSOAcleaning.R
@@ -1,15 +1,16 @@
 library(tidyverse)
 
-CleanedHousePrices = read.csv("C:/Users/Acer/Documents/ST5014CEM_DataScienceForDevelopers_PrajwolKhadka_230225/Cleaned Data/Cleaned_Town_Population.csv")
+#Reading data-sets
+CleanedHousePrices = read.csv("C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Cleaned Data\\Cleaned_Town_Population.csv")
 
-LSOA = read_csv("C:/Users/Acer/Documents/ST5014CEM_DataScienceForDevelopers_PrajwolKhadka_230225/Obtained Data/PostcodeToLSOA/Postcode to LSOA.csv", show_col_types = FALSE)
+LSOA = read_csv("C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Obtained Data\\PostcodeToLSOA\\Postcode to LSOA.csv", show_col_types = FALSE)
 
 
 pattern = ' .*$'
 LSOA_Cleaned = LSOA %>%
   select(lsoa11cd, pcds) %>%
   mutate(shortPostcode = gsub(pattern, "", pcds)) %>%
-  right_join(Cleaned_HP, by = "shortPostcode") %>%
+  right_join(CleanedHousePrices, by = "shortPostcode") %>%
   group_by(lsoa11cd) %>%
   select(lsoa11cd, shortPostcode, Town, District, County,
          Population2020, Population2021, Population2022, Population2023, Population2024) %>%
@@ -20,4 +21,4 @@ colnames(LSOA_Cleaned)[1] = "LSOA_code"
 
 View(LSOA_Cleaned)
 
-write.csv(LSOA_Cleaned, "C:/Users/Acer/Documents/ST5014CEM_DataScienceForDevelopers_PrajwolKhadka_230225/Cleaned Data/Cleaned_LSOA.csv", row.names = FALSE)
+write.csv(LSOA_Cleaned, "C:\\Users\\ADMIN\\Desktop\\Data Science Assignment\\Cleaned Data\\Cleaned_LSOA.csv", row.names = FALSE)
